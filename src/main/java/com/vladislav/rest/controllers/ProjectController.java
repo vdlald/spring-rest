6b4c8a23d120d4816e5e1f23c6559c1690680618
Vladislav Golubinov <vlad.vdlald@gmail.com>
2020-07-24 16:08:15 +0300

    projects hypermedia

--- src/main/java/com/vladislav/rest/controllers/ProjectController.java
+++ src/main/java/com/vladislav/rest/controllers/ProjectController.java
@@ -6,43 +6,60 @@ import com.vladislav.rest.models.Task;
 import com.vladislav.rest.services.ProjectService;
 import com.vladislav.rest.utils.BeanUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequiredArgsConstructor
 public class ProjectController {
 
     private final ProjectService service;
+    private final RepresentationModelAssembler<Project, EntityModel<Project>> modelAssembler;
 
     @GetMapping("/projects")
-    public List<Project> getAllProjects() {
-        return service.getAll();
+    public CollectionModel<EntityModel<Project>> getAllProjects() {
+        final List<EntityModel<Project>> projects = service.getAll().stream()
+                .map(modelAssembler::toModel)
+                .collect(Collectors.toUnmodifiableList());
+        final Link selfRel = linkTo(methodOn(ProjectController.class).getAllProjects()).withSelfRel();
+        return CollectionModel.of(projects, selfRel);
     }
 
     @GetMapping("/projects/{id}")
-    public Project getProjectById(@PathVariable Long id) {
-        return service.getById(id);
+    public EntityModel<Project> getProjectById(@PathVariable Long id) {
+        final Project project = service.getById(id);
+        return modelAssembler.toModel(project);
     }
 
     @PostMapping("/projects")
     @ResponseStatus(HttpStatus.CREATED)
-    public Project createProject(@RequestBody Project project) {
-        return service.save(project);
+    public EntityModel<Project> createProject(@RequestBody Project incomingProject) {
+        final Project project = service.save(incomingProject);
+        return modelAssembler.toModel(project);
     }
 
     @PutMapping("/projects/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Project putProject(@RequestBody Project incomingDto, @PathVariable Long id) {
+    public EntityModel<Project> putProject(@RequestBody Project incomingDto, @PathVariable Long id) {
+        Project saved;
         try {
             final Project project = service.getById(id);
             BeanUtils.copyPropertiesExcludeNullProperties(incomingDto, project);
-            return service.save(project);
+            saved =  service.save(project);
         } catch (ResourceNotFoundException ignore) {
-            return service.save(incomingDto);
+            saved = service.save(incomingDto);
         }
+        return modelAssembler.toModel(saved);
     }
 
     @DeleteMapping("/projects/{id}")
